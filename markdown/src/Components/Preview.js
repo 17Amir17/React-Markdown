4ff58b7b1847e7288f794e79ec6fdb79f24c374f
17Amir17 <amirangel101@hotmail.com>
2021-11-29 15:42:58 +0200

    Added cool code and table styling

--- markdown/src/Components/Preview.js
+++ markdown/src/Components/Preview.js
@@ -1,12 +1,29 @@
 import { useContext } from 'react';
 import { EditorTextContext } from '../Context/EditorTextContext';
 import Markdown from 'marked-react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import '../styles/preview.css';
+
+const renderer = {
+  code: (snippet, lang) => {
+    return (
+      <SyntaxHighlighter language={lang} style={a11yDark}>
+        {snippet}
+      </SyntaxHighlighter>
+    );
+  },
+};
 
 function Preview(props) {
   const context = useContext(EditorTextContext);
   return (
     <div id="preview">
-      <Markdown breaks={true}>{context.text}</Markdown>
+      <Markdown
+        breaks={true}
+        value={context.text}
+        renderer={renderer}
+      ></Markdown>
     </div>
   );
 }
